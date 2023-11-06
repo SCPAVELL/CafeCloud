d3da4ef032a2cbdc55683338aed67cd98fee72a4
Pavel <pavel2001stepanov@yandex.ru>
2023-11-06 16:52:37 +0300

    Cafe step 1

---
+++ src/main/java/controller/Taco.java
@@ -0,0 +1,13 @@
+package controller;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class Taco {
+
+	private String name;
+	private List<Ingredient> ingredients;
+
+}
---
+++ src/test/java/sia/cafecloud/CafeCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package sia.cafecloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
